ab4f0abdbe8f6a611aead0ac451e7cc7280ed1e4
chrisloarryn <cristobal.contreras11@inacapmail.cl>
2020-05-06 02:28:28 -0400

    port

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -57,3 +57,26 @@ const Footer = () => {
 }
 
 export default Footer
+
+
+//5. If you used npm, install yarn (http://yarnpkg.com/) and repeat the above steps with it instead.
+// This may help because npm has known issues with package hoisting which may get resolved in future versions.
+
+// 6. Check if /Users/developmac/Documents/GitHub/portfolio-react/node_modules/webpack is outside your project directory.
+//    For example, you might have accidentally installed something in your home folder.
+
+// 7. Try running npm ls webpack in your project folder.
+//    This will tell you which other package (apart from the expected react-scripts) installed webpack.
+
+// If nothing else helps, add SKIP_PREFLIGHT_CHECK=true to an .env file in your project.
+// That would permanently disable this preflight check in case you want to proceed anyway.
+
+// P.S. We know this message is long but please read the steps above :-) We hope you find them helpful!
+
+// npm ERR! code ELIFECYCLE
+// npm ERR! errno 1
+// npm ERR! portfolio-react@0.1.0 build: `react-scripts build`
+// npm ERR! Exit status 1
+// npm ERR! 
+// npm ERR! Failed at the portfolio-react@0.1.0 build script.
+// npm ERR! This is probably not a problem with npm. There is likely additional logging output above.
